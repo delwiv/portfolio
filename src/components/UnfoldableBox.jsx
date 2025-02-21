5e1bf298bc120586949916cd5f23fd46a6e302f8
Louis Cathala <delwiv@protonmail.com>
2025-02-21 17:33:24 +0100

    feat: animate fold/unfold

--- src/components/UnfoldableBox.jsx
+++ src/components/UnfoldableBox.jsx
@@ -7,11 +7,11 @@ export default function UnfoldableBox({ children, height = '84px' }) {
   const [unfold, setUnfold] = useState(false)
 
   return (
-    <div >
-      <div className={clsx(!unfold && "line-clamp-3")}>
+    <div>
+      <div className={clsx(unfold ? 'max-h-screen' : "max-h-[63px]", "transition-all overflow-y-hidden duration-500 ease-in-out")}>
         {children}
       </div>
-      <div className="cursor-pointer" onClick={() => setUnfold(unfold => !unfold)}>View {unfold ? "less" : "more"}...</div>
+      <div className="cursor-pointer underline " onClick={() => setUnfold(unfold => !unfold)}>View {unfold ? "less" : "more"}</div>
     </div>
   )
 
