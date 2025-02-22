b5df1a932ce1fb2abd36590c2516b72c0c6d9937
Louis Cathala <delwiv@protonmail.com>
2025-02-22 22:25:03 +0100

    feat: use grid for SSR and masonry on client

--- src/components/MasonryGrid.jsx
+++ src/components/MasonryGrid.jsx
@@ -1,7 +1,35 @@
-export default function MasonryGrid({ children }) {
-  return (
+'use client'
+
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+
+export default function MasonryGrid({
+  children,
+  columnsCountBreakPoints = {
+    349: 1,
+    767: 2,
+    1535: 3,
+  },
+}) {
+  const isServer = typeof window === 'undefined'
+  console.log({ isServer })
+  // return (
+  //   <ResponsiveMasonry
+  //     columnsCountBreakPoints={columnsCountBreakPoints}
+  //     gutterBreakpoints={{ 350: '1rem' }}
+  //   >
+  //     <Masonry>{children}</Masonry>
+  //   </ResponsiveMasonry>
+  // )
+  return isServer ? (
     <div className='grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 gap-[10px]'>
       {children}
     </div>
+  ) : (
+    <ResponsiveMasonry
+      columnsCountBreakPoints={columnsCountBreakPoints}
+      gutterBreakpoints={{ 350: '1rem' }}
+    >
+      <Masonry>{children}</Masonry>
+    </ResponsiveMasonry>
   )
 }
