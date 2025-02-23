7a513e4fa8d06b70b0dd1f67de0543af5c0d886b
Louis Cathala <delwiv@protonmail.com>
2025-02-23 18:57:37 +0100

    feat: small footer tweaks for mobile

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,26 +1,20 @@
 export default function Footer({ settings }) {
   return (
     <div className='p-8 flex flex-col gap-4'>
-      <div className='w-full flex justify-between gap-8'>
-        <div className='flex md:gap-8 gap-4 w-full justify-start'>
+      <div className='w-full flex md:flex-row flex-col justify-center md:justify-between gap-8'>
+        <div className='flex md:gap-8 gap-4 w-full justify-center md:justify-start'>
           {settings.socialLinks.map((social) => (
             <a key={social.platform} className='underline' href={social.url}>
               {social.platform}
             </a>
           ))}
         </div>
-        <div className='hidden md:block w-full text-right'>
+        <div className='w-full md:text-right text-center'>
           This app is open source and available on{' '}
           <a className='underline' href='https://github.com/delwiv/portfolio'>
             GitHub
           </a>
         </div>
-      </div>
-      <div className='md:hidden w-full text-center'>
-        This app is open source and available on{' '}
-        <a className='underline' href='https://github.com/delwiv/portfolio'>
-          GitHub
-        </a>
       </div>
     </div>
   )
