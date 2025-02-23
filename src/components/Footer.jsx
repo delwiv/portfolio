3526a9589d5642aa921ed4a43b84c98a92295948
Louis Cathala <delwiv@protonmail.com>
2025-02-23 18:40:02 +0100

    feat: improve mobile footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,22 @@
 export default function Footer({ settings }) {
   return (
-    <div className='w-full flex justify-between p-12 gap-8'>
-      <div className='flex gap-8'>
-        {settings.socialLinks.map((social) => (
-          <a key={social.platform} className='underline' href={social.url}>
-            {social.platform}
+    <div className='p-8 flex flex-col gap-4'>
+      <div className='w-full flex justify-between gap-8'>
+        <div className='flex md:gap-8 gap-4 w-full justify-start'>
+          {settings.socialLinks.map((social) => (
+            <a key={social.platform} className='underline' href={social.url}>
+              {social.platform}
+            </a>
+          ))}
+        </div>
+        <div className='hidden md:block w-full text-right'>
+          This app is open source and available on{' '}
+          <a className='underline' href='https://github.com/delwiv/portfolio'>
+            GitHub
           </a>
-        ))}
+        </div>
       </div>
-      <div>
+      <div className='md:hidden w-full text-center'>
         This app is open source and available on{' '}
         <a className='underline' href='https://github.com/delwiv/portfolio'>
           GitHub
