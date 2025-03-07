a73dcb4b0a86a6995949578e33216ab0fe828686
Louis Cathala <delwiv@protonmail.com>
2025-03-07 18:12:37 +0100

    feat: encoreURIComponents

--- src/utils/summary.js
+++ src/utils/summary.js
@@ -1,4 +1,5 @@
-export const getHeadingId = (children) => children.map((c) => CSS.escape(c))
+export const getHeadingId = (children) =>
+  children.map((c) => encodeURIComponent(c))
 
 export const extractSummary = (post) =>
   [{ style: 'h1', children: [{ text: post.title }] }].concat(
