dff7a8284b113a8ec83fd97950b14dda64b1e946
Louis Cathala <delwiv@protonmail.com>
2025-03-08 13:31:33 +0100

    feat: force https protocol and rm from headers

--- src/middleware.js
+++ src/middleware.js
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server'
 export function middleware(request) {
   const response = NextResponse.next()
   response.headers.set('pathname', request.nextUrl.pathname)
-  response.headers.set('proto', request.nextUrl.protocol)
 
   return response
 }
--- src/utils/headers.js
+++ src/utils/headers.js
@@ -4,11 +4,10 @@ export const parseHeaders = async () => {
   const head = await headers()
   const origin = head.get('host')
   const pathname = head.get('pathname') || ''
-  const proto = head.get('proto')
 
   const slug = pathname.split('/').pop()
 
-  let url = new URL(`${proto}//${origin}`).toString()
+  let url = new URL(`https://${origin}`).toString()
 
   return { url, pathname, slug }
 }
