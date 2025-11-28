eb6cdf4acf930086a56b2a2c3121cacf8650f3a1
Louis Cathala <delwiv@protonmail.com>
2025-11-28 03:43:53 +0100

    hide language switcher if no translations

--- src/components/LanguagePicker.jsx
+++ src/components/LanguagePicker.jsx
@@ -12,40 +12,43 @@ export default function LanguagePicker({ language }) {
   const [show, setShow] = useState(false)
 
   return (
-    <div className='group mr-4'>
-      <Image
-        className='cursor-pointer'
-        onClick={() => setShow((show) => !show)}
-        src={`/flag-${language}.svg`}
-        width={30}
-        height={20}
-        alt={language}
-      ></Image>
-      {translations?.length > 0 && (
-        <div
-          className={clsx(
-            'absolute top-16 group mr-4 right-0 flex-col gap-4 bg-black/50 p-4 rounded-lg',
-            show ? 'flex' : 'hidden'
-          )}
-        >
-          {translations
-            .filter((loc) => loc.language !== language)
-            .map((loc) => (
-              <Link
-                key={loc.newPath}
-                href={loc.newPath}
-                className='w-[30px] h-[20px]'
-              >
-                <Image
-                  src={`/flag-${loc.language}.svg`}
-                  width={30}
-                  height={20}
-                  alt={loc}
-                ></Image>
-              </Link>
-            ))}
-        </div>
-      )}
-    </div>
+    translations?.length > 0 && (
+      <div className='group mr-4'>
+        <Image
+          className='cursor-pointer'
+          onClick={() => setShow((show) => !show)}
+          src={`/flag-${language}.svg`}
+          width={30}
+          height={20}
+          alt={language}
+        ></Image>
+        {translations?.length > 0 && (
+          <div
+            className={clsx(
+              'absolute top-16 group mr-4 right-0 flex-col gap-4 bg-black/50 p-4 rounded-b-lg',
+              show ? 'flex' : 'hidden'
+            )}
+          >
+            {translations
+              .filter((loc) => loc.language !== language)
+              .map((loc) => (
+                <Link
+                  onClick={() => setShow(false)}
+                  key={loc.newPath}
+                  href={loc.newPath}
+                  className='w-[30px] h-[20px]'
+                >
+                  <Image
+                    src={`/flag-${loc.language}.svg`}
+                    width={30}
+                    height={20}
+                    alt={loc}
+                  ></Image>
+                </Link>
+              ))}
+          </div>
+        )}
+      </div>
+    )
   )
 }
