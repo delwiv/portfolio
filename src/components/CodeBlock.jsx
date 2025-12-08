630fdbb7678f63c8ec9539e01b6fba69ca9227d0
Louis Cathala <delwiv@protonmail.com>
2025-12-08 20:33:21 +0100

    update max blog content width

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -24,7 +24,7 @@ export default function CodeBlock({ code, language }) {
   }, [code])
 
   return (
-    <div className='max-w-[100vw] overflow-x-scroll relative codeblock'>
+    <div className='lg:max-w-[75vw] overflow-x-scroll relative codeblock'>
       <div
         className={clsx(
           'rounded-xl absolute right-1 top-3 cursor-pointer transition-colors duration-500 p-2',
