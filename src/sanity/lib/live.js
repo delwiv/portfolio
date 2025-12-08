773c249de24fce0ffdbc75c21bf67e41addddacc
Louis Cathala <delwiv@protonmail.com>
2025-12-08 20:15:45 +0100

    Update deps

--- src/sanity/lib/live.js
+++ src/sanity/lib/live.js
@@ -1,13 +1,17 @@
 // Querying with "sanityFetch" will keep content automatically updated
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
-import { defineLive } from 'next-sanity'
-import { client } from './client'
+import { createClient } from 'next-sanity'
+import { defineLive } from 'next-sanity/live'
+
+// Import environment variables
+import { apiVersion, dataset, projectId } from '../env'
 
 export const { sanityFetch, SanityLive } = defineLive({
-  client: client.withConfig({
-    // Live content is currently only available on the experimental API
-    // https://www.sanity.io/docs/api-versioning
-    apiVersion: 'vX',
+  client: createClient({
+    projectId,
+    dataset,
+    apiVersion,
+    useCdn: true,
   }),
 })
